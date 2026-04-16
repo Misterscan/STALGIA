31c66034765710259a2419ed133485b84a1b02dc
Misterscan <misterscanmusic@aol.com>
2026-04-16 16:43:43 -0400

    fix: add macOS Apple Silicon support for FluidSynth library detection

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
+import os
+import sys
+
+# macOS Apple Silicon (Homebrew) fix for finding FluidSynth
+if sys.platform == 'darwin' and os.path.isdir('/opt/homebrew/lib'):
+    # Prepend Homebrew's lib dir so ctypes.util.find_library can find it
+    os.environ['DYLD_FALLBACK_LIBRARY_PATH'] = '/opt/homebrew/lib' + (
+        ':' + os.environ['DYLD_FALLBACK_LIBRARY_PATH'] if 'DYLD_FALLBACK_LIBRARY_PATH' in os.environ else ''
+    )
+
 from app import create_app
 
 app = create_app()
